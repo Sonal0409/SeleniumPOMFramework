458210e0ce2d4faa4710512c94b47c2b2ee9eb8f
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2021-08-29 18:47:33 +0530

    Update HomePage.java

--- src/main/java/com/wiki/qa/pages/HomePage.java
+++ src/main/java/com/wiki/qa/pages/HomePage.java
@@ -8,13 +8,22 @@ import com.wiki.qa.base.TestBase;
 
 public class HomePage extends TestBase{
 	
-	// PageFactory
+	//@FindBy: An annotation used in Page Factory to locate and declare web elements using different locators.
+	
 	
 	@FindBy(id="searchInput")
 	WebElement searchinputbox;
 	
 	@FindBy(xpath="//*[@id=\"search-form\"]/fieldset/button")
 	WebElement searchbutton;
+	
+	// PageFactory
+	//Page Factory is a class provided by Selenium WebDriver to support Page Object Design patterns. 
+	//In Page Factory, testers use @FindBy annotation. The initElements method is used to initialize web elements.
+	// driver is coming from Testbase class
+	// "this" here means.. initialize all the variables in the current class with this driver
+	// you can also write Homepage.class instad of this..
+	//this means its pointing to the current class object
 	
     public HomePage()
     {
